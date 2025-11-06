98695cc796278bb4577e6a95332505c0dc809dc3
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-11-06 09:24:31 +0530

    redis

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import express from "express"
+const app = express();
+import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
+import radis from 'ioredis'
+
+const PORT = process.env.PORT || 4000
+
+// const cacheStore = {
+//     totalPageCount: 0
+// } // issues ---> clear cache, Not distributed,  LRU(least recent use), Server crach,----> (set of problems Unloock)
+
+
+const redis = new radis({ host: "localhost", port: 6379 })
+
+app.get("/books", async (req , res) => {
+
+    // check cache
+    // if(cacheStore.totalPageCount) {
+    //     console.log("cache hit")
+    //     return res.json({totalPageCount: cacheStore.totalPageCount})
+    // }
+    const cachedValue = await redis.get("totalPageValue")
+    if(cachedValue) {
+        return res.json({ totalPageCount: cachedValue})
+    }
+    
+    const response = await axios.get("https://api.freeapi.app/api/v1/public/books")
+    // return res.json(response.data);
+    const totalPageCount = response?.data?.data?.data?.reduce((acc, curr) => 
+        !curr.volumeInfo?.pageCount ? 0 : curr.volumeInfo?.pageCount + acc,
+    0
+    );
+    // set the cache
+    // cacheStore.totalPageCount = totalPageCount;
+    await redis.set("totalPageValue", totalPageCount)
+    console.log("cache miss")
+    return res.json(totalPageCount);
+})
+
+
+app.listen(PORT , () => {
+    console.log(`Server is running on PORT: ${PORT}`)
+});
+
+// https://api.freeapi.app/api/v1/public/books
